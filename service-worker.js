77e8b2e10145f6a587e6df4c85915b10986b8fd0
kin-jpg-arch <changunheo6@gmail.com>
2026-02-04 16:49:14 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE="pomodoro-cache-v1";
-const FILES=["./","./index.html","./style.css","./script.js","./manifest.json"];
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-});
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
